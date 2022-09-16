e91a783f75f1174bf787d9e7014bbca1a4ac21f9
Nicolas Cavallin <nicavallin@gmail.com>
2022-09-16 08:28:45 +0200

    more path alias

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,9 @@ module.exports = {
       {
         alias: {
           "@screens": "./src/screens",
+          "@components/*": ["src/components/*"],
+          "@theme/*": ["src/theme/*"],
+          "@utils/*": ["src/utils/*"],
         },
       },
     ],
