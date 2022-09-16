4fd51be7a46f1bd2e2c5f88112cac815d842feea
Nicolas Cavallin <nicavallin@gmail.com>
2022-09-16 08:13:25 +0200

    my environment setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,61 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: "@react-native-community",
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "import"],
+  rules: {
+    quotes: ["error", "double"],
+    "react/jsx-indent": ["warn", 2, { indentLogicalExpressions: false }],
+    "react/jsx-sort-props": [
+      "error",
+      {
+        callbacksLast: true,
+        shorthandLast: true,
+        ignoreCase: true,
+        noSortAlphabetically: false,
+      },
+    ],
+    "sort-imports": [
+      "error",
+      { ignoreCase: true, ignoreDeclarationSort: true },
+    ],
+    // this is for sorting imports
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["external", "builtin"],
+          "internal",
+          ["sibling", "parent"],
+          "index",
+        ],
+        pathGroups: [
+          {
+            pattern: "@(react|react-native)",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@src/**",
+            group: "internal",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["internal", "react"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ["*.ts", "*.tsx"],
       rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
+        "@typescript-eslint/no-shadow": ["error"],
+        "no-shadow": "off",
+        "no-undef": "off",
       },
     },
   ],
