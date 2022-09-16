4b095aae58c3f0a5317f81eeba0ed23e129939fc
Nicolas Cavallin <nicavallin@gmail.com>
2022-09-16 10:30:57 +0200

    added theme files

---
+++ src/theme/colors.ts
@@ -0,0 +1,9 @@
+export default {
+  white: "#FFFFFF",
+  bodyGray: "#F3F4F6",
+  blackText: "#1F2937",
+  grayText: "#808692",
+  grayIcon: "#434D5C",
+  blueAccent: "#4181F2",
+  grayLine: "#CFD3D9",
+};
