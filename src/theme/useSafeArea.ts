0f2e65695ad62ede39553d6f31f02f2eebd16a92
Nicolas Cavallin <nicavallin@gmail.com>
2022-09-21 11:34:40 +0200

    fixed bottom padding when safe area is zero

--- src/theme/useSafeArea.ts
+++ src/theme/useSafeArea.ts
@@ -4,6 +4,7 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const HEADER_HEIGHT = 62;
 const SMALL_MARGIN = 8;
+const MEDIUM_MARGIN = 16;
 
 export default () => {
   const { top, bottom } = useSafeAreaInsets();
@@ -12,7 +13,7 @@ export default () => {
     () => ({
       headerHeight: HEADER_HEIGHT + top,
       paddingTop: top,
-      paddingBottom: bottom,
+      paddingBottom: bottom > 0 ? bottom : MEDIUM_MARGIN,
       listPaddingBottom: bottom + SMALL_MARGIN,
     }),
     [top, bottom],
